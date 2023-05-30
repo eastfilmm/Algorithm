a3bdf40fa9198e1b9f3eb4cb76e9e08b745c1453
eastfilmm <91816664+eastfilmm@users.noreply.github.com>
2023-05-30 20:24:23 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,26 @@
+def solution(n, k):
+    answer = 0
+    
+    num = ""
+    
+    while n>0:
+        tmp = n%k
+        num= str(tmp)+num
+        n=n//k
+    
+    list = num.split("0")
+    isTrue = 0
+    
+    for i in list:
+        if len(i)==0:
+            continue
+        if int(i)<2:
+            continue
+        for j in range(2,int(int(i)**0.5)+1):
+            if int(i)%j==0:
+                isTrue = 1
+                break;
+        if isTrue !=1:
+            answer+=1
+            
+    return answer
